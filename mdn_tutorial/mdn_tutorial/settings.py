3180a6e17c572582ef6a31d3adb0b6337faededf
liewchooichin <liewchooichin@gmail.com>
2024-06-16 16:13:10 +0800

    added app book_catalog

--- mdn_tutorial/mdn_tutorial/settings.py
+++ mdn_tutorial/mdn_tutorial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # add the book_catalog app
+    'book_catalog.apps.BookCatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kuala_Lumpur'
 
 USE_I18N = True
 
